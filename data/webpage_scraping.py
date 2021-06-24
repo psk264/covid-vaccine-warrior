923364f4ae0ad43370283e4196c192c406ec9cdb
psk264 <84349071+psk264@users.noreply.github.com>
2021-06-24 00:23:39 -0400

    scrape website data into list of dict and write it into csv

--- data/webpage_scraping.py
+++ data/webpage_scraping.py
@@ -1,18 +1,26 @@
+import datetime
 from selenium import webdriver
-from webdriver_manager import driver
+from selenium.webdriver.common.utils import is_url_connectable
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import os
 
 def website_scraper(URL):
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options) #https://pypi.org/project/webdriver-manager/
     # driver = webdriver.Chrome(executable_path=os.path.join(os.path.dirname(__file__), "..", "chromedriver.exe"), options=options)
     
     driver.get(URL)
+    wait = WebDriverWait(driver, 60)
+    # element = driver.find_element_by_css_selector('#availableSitesLabel > span')
+    total_count_element = wait.until(EC.presence_of_element_located((By.LINK_TEXT,"Directions")))
+
     
 
     print(type(driver))
@@ -45,17 +53,40 @@ def website_scraper(URL):
                     single_facility_dict["facility_type"] = temp_list[i]
                 elif i == 2 :
                     single_facility_dict["address"] = temp_list[i]
-                    single_facility_dict["zip_code"] = temp_list[i].split(' ')[-5:0]
+                    single_facility_dict["zip_code"] = temp_list[i][-5:]
                 elif i == 3 :
                     single_facility_dict["phone_number"] = temp_list[i]
                 elif i == 5 and len(temp_list) == 10 :  #statement to handle multiple vaccine type
-                    single_facility_dict["vaccines_offered"] = temp_list[i] + "," + temp_list[i+1]
+                    vaxtype = temp_list[i]
+                    if("Pfizer" in temp_list[i+1] or "Johnson" in temp_list[i+1] or "Moderna" in temp_list[i+1]):
+                        vaxtype = temp_list[i] + "," + temp_list[i+1]
+                    single_facility_dict["vaccines_offered"] = vaxtype
                 elif i == 5 :
                     single_facility_dict["vaccines_offered"] = temp_list[i]
                 elif i == 7:
                     single_facility_dict["availability"] = temp_list[i]
-        facility_list.append(single_facility_dict)
+
+            facility_list.append(single_facility_dict)
                     
     print(type(facility_list))
     print(len(facility_list))
-    print(facility_list[0])
+    # print(facility_list[0])
+    store_data_csv(facility_list)
+
+def store_data_csv(facility_list):
+    if(len(facility_list)>0):
+        dir_name = "data"
+        try:
+            os.mkdir("data")        
+        except FileExistsError:
+            print("Direcotry \'data\' already exists!")
+
+        current_date = datetime.datetime.now()
+        file_name = os.path.join(os.path.dirname(__file__), "..", "data", "facility_data.csv")
+        print(file_name)
+
+        with open(file_name, "w") as file:
+            file.write(str(facility_list))
+    else:
+        print("Sorry, file cannot be save! No data found.")
+        exit()
