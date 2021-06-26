262a0c46726e31eee6edf6a45532bbf1e470a85c
SnowZhao321 <64279935+SnowZhao321@users.noreply.github.com>
2021-06-26 17:19:23 -0400

    Fixed bugs in Web Design

--- app/vaccine_finder.py
+++ app/vaccine_finder.py
@@ -8,19 +8,16 @@ URL = os.getenv("URL", default="Incorrect URL, please set env var called 'URL'")
 
 #website_scraper(URL)
 
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "../data/facility_data.json"
+abs_file_path = os.path.join(script_dir, rel_path)
 
-facility_list = [{'name_of_venue': 'Bedford-Stuyvesant Restoration Corporation', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+),Johnson & Johnson (18+)', 'availability': 'FIRST DOSE APPOINTMENTS AVAILABLE!', 'address': '1368 Fulton Street, Community Room, Brooklyn, 11216', 'zip_code': '11216', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'BMS Family Health Center at St. Paul Community Baptist Church', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+)', 'availability': 'FIRST DOSE APPOINTMENTS AVAILABLE!', 'address': '859 Hendrix Street, Brooklyn, 11207', 'zip_code': '11207', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Bronx Co-Op City Dreiser Community Center', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Johnson & Johnson (18+)', 'availability': 'FIRST DOSE APPOINTMENTS AVAILABLE!', 'address': '177 Dreiser Loop, Bronx, 10475', 'zip_code': '10475', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Bronx Zoo Vaccination Site', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+)', 'availability': 'Walk-up vaccinations available to all eligible New Yorkers', 'address': '2300 Southern Boulevard, Bronx, 10460', 'zip_code': '10460', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Christian Cultural Center', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+)', 'availability': 'FIRST DOSE APPOINTMENTS AVAILABLE!', 'address': '12020 Flatlands Avenue, Brooklyn, 11207', 'zip_code': '11207', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Church of God East New York', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+)', 'availability': 'Schedule appointment', 'address': '905 Sutter Avenue, Brooklyn, 11207', 'zip_code': '11207', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Church of the Holy Apostles', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Pfizer (12+)', 'availability': 'FIRST DOSE APPOINTMENTS AVAILABLE!', 'address': '296 9th Avenue, Manhattan, 10001', 'zip_code': '10001', 'phone_number': '(877) 829-4692'}, 
-{'name_of_venue': 'Coney Island YMCA', 'facility_type': 'Community Health Center/Clinic', 'vaccines_offered': 'Moderna (18+)', 'availability': 'Walk-up vaccinations available to all eligible New Yorkers', 'address': '2980 West 29th Street, Brooklyn, 11224', 'zip_code': '11224', 'phone_number': '(877) 829-4692'}
-]
+openfile = open(abs_file_path)
+import json
+facility_list = json.load(openfile)
 
 
-import os
+
 import json
 from pprint import pprint
 from dateutil.parser import parse as parse_datetime
@@ -37,7 +34,7 @@ from operator import itemgetter
 
 def vaccine_stop(zipcode):
     
-    user_zip = zipcode
+    user_zip = int(zipcode)
 
     search = SearchEngine(simple_zipcode=True)
 
@@ -50,6 +47,10 @@ def vaccine_stop(zipcode):
         zip2 =search.by_zipcode(n["zip_code"])
         lat2 =zip2.lat
         long2 =zip2.lng
+
+        if lat2 is None: # some zipcode returns Null lat and long
+            continue
+
         distance = float(mpu.haversine_distance((lat1,long1),(lat2,long2)))
         distance_add.append({
                 "name_of_venue": n["name_of_venue"],
@@ -64,9 +65,9 @@ def vaccine_stop(zipcode):
 
         
     facility_list_sorted = sorted(distance_add, key=itemgetter('distance')) 
-    facility_list_final = facility_list_sorted[0:25]
+    facility_list_final = facility_list_sorted[0:10]
     result="\n\n\n"
     for f in facility_list_final:
-        result = result + f["name_of_venue"] + "\n" + f["facility_type"] + "\n" + f["address"] + "\n" + "Distance: " + str(f["distance"]) + " Miles" + "\n" + "Vaccine Type: " + f["vaccines_offered"] + "\n" + f["availability"] + "\n" + f["phone_number"] + "\n\n\n"
-    
+        result = result + f["name_of_venue"] + "\n" + f["facility_type"] + "\n" + f["address"] + "\n" + "Distance: " + str(f["distance"]) + " Miles" + "\n" + "Vaccine Type: " + f["vaccines_offered"] + "\n" + f["availability"] + "\n" + "Phone: " + str(f["phone_number"]) + "\n\n\n"
+
     return(result)
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -6,25 +6,21 @@ from app.vaccine_finder import vaccine_stop
 
 home_routes = Blueprint("home_routes", __name__)
 
-@home_routes.route("/home")
-def index():
-    return render_template("home.html")
-
-@home_routes.route("/about")
-def about():
-    return render_template("about.html")
-
 @home_routes.route("/")
 @home_routes.route("/vaccine_finder")
 def vaccine_finder():
     return render_template("vaccine.html")
 
-@home_routes.route("/search_result", methods=["GET", "POST"])
+@home_routes.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@home_routes.route("/getinput", methods=["GET", "POST"])
 def search_result():
-    print("FORM DATA: ", dict(request.args))
-    request_data = dict(request.args)
-    country_code = request_data.get("country_code") or "US"
-    zip_code = request_data.get("zip_code") or "20057"
+    print("FORM DATA: ", dict(request.form))
+    request_data = dict(request.form)
+    zip_code = request_data.get("zip_code")
     print("zip code: ", zip_code)
     results = vaccine_stop(zipcode=zip_code)
     print(results)
