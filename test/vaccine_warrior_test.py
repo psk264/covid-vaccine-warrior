0207e2f330ec4ff5c73c9f4d61a7b2a4856ca0fb
psk264 <84349071+psk264@users.noreply.github.com>
2021-06-27 22:54:47 -0400

    fixed test cases based on new error handling

--- test/vaccine_warrior_test.py
+++ test/vaccine_warrior_test.py
@@ -19,23 +19,22 @@ def test_website_scraper_url():
 #Test graceful failure when non-numeric characters are input instead of zipcode
 def test_vaccine_stop_random_string():
     result = vaccine_stop("random string")
-    assert result in "Invalid Zip Code. Please Check Your Inputs and Try Again."
+    assert "Your Input random string is not a Valid Zip Code. Please Check Your Inputs and Try Again." in result 
 
 #Test graceful failure when numeric characters are input but its not a valid zipcode
 def test_vaccine_stop_invalid_zipcode():
     result = vaccine_stop("11111")
-    assert result in "Invalid Zip Code. Please Check Your Inputs and Try Again."
+    assert "Your Input 11111 is not a Valid Zip Code. Please Check Your Inputs and Try Again." in result 
 
 #Test graceful failure when numeric characters are input but its not a valid zipcode format
 def test_vaccine_stop_zipcode_len():
     result = vaccine_stop("123456")
-    assert result in "Invalid Zip Code. Please Check Your Inputs and Try Again."
-
+    assert "Your Input 123456 is not a Valid Zip Code. Please Check Your Inputs and Try Again." in result 
 
 #Test graceful failure when the distance between zip code is greater 50 miles
 def test_vaccine_stop_50_miles():
     result = vaccine_stop("18015")
-    assert result in "No Results Within 50 Miles of Your Location"
+    assert "No Results Within 50 Miles of Your Input Zip Code 18015. Please Enter a Valid NYC Zip Code." in result 
 
 
 #Test output when valid zipcode is entered
